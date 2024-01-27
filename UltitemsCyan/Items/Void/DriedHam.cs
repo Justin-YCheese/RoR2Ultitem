13a569fb29fd13c3496216c0f0c7a9e37f25b0ed
Justin Young <jjyusa2@gmail.com>
2024-01-27 13:40:29 -0500

    Dried Ham nerf - Flat healing stacks

--- UltitemsCyan/Items/Void/DriedHam.cs
+++ UltitemsCyan/Items/Void/DriedHam.cs
@@ -14,8 +14,8 @@ namespace UltitemsCyan.Items.Void
         public static ItemDef item;
         public static ItemDef transformItem;
         private const float threshold = 30f;
-        private const float percentHealing = 2f;
-        private const float flatHealing = 8f;
+        private const float percentHealing = 1f;
+        private const float flatHealing = 4f;
 
         private void Tokens()
         {
@@ -23,7 +23,7 @@ namespace UltitemsCyan.Items.Void
 
             LanguageAPI.Add(tokenPrefix + "_NAME", "Dried Ham");
             LanguageAPI.Add(tokenPrefix + "_PICK", "Heal when hitting enemies below 30% health.");
-            LanguageAPI.Add(tokenPrefix + "_DESC", "<style=cIsHealing>Heal</style> for <style=cIsHealing>4</style> plus an additional <style=cIsHealing>1%</style> <style=cStack>(+1% per stack)</style> when dealing damage to enemies below <style=cIsDamage>30% health</style>. Corrupts all Crème Brûlée.");
+            LanguageAPI.Add(tokenPrefix + "_DESC", "<style=cIsHealing>Heal</style> for <style=cIsHealing>1%</style> plus an additional <style=cIsHealing>4</style> <style=cStack>(+4 per stack)</style> when dealing damage to enemies below <style=cIsDamage>30% health</style>. Corrupts all Crème Brûlée.");
             LanguageAPI.Add(tokenPrefix + "_LORE", "The bitter aftertaste is just the spoilage");
 
             item.name = tokenPrefix + "_NAME";
@@ -102,7 +102,8 @@ namespace UltitemsCyan.Items.Void
                         if (self.combinedHealthFraction <= threshold / 100f)
                         {
                             //Log.Debug("Heal Attacker, Initial: " + inflictor.healthComponent.health);
-                            inflictor.healthComponent.Heal(inflictor.healthComponent.fullHealth * percentHealing / 100f * grabCount + flatHealing, damageInfo.procChainMask);
+                            inflictor.healthComponent.Heal(inflictor.healthComponent.fullHealth * percentHealing / 100f + flatHealing * grabCount, damageInfo.procChainMask);
+                            //inflictor.healthComponent.Heal(inflictor.healthComponent.fullHealth * percentHealing / 100f * grabCount + flatHealing, damageInfo.procChainMask);
                             Log.Debug("Healing: " + (inflictor.healthComponent.fullHealth * percentHealing / 100f + flatHealing));
                             // TODO Change sound
                             Util.PlaySound("Play_item_proc_thorns", inflictor.gameObject);
